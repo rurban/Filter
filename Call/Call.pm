3ba4445e55f8bcefb3030d9b9dd9c52ecee732b2
Reini Urban <rurban@cpanel.net>
2017-01-22 22:10:49 -0600

    1.57 released
    no changes

--- Call/Call.pm
+++ Call/Call.pm
@@ -19,7 +19,7 @@ use vars qw($VERSION $XS_VERSION @ISA @EXPORT) ;
 
 @ISA = qw(Exporter DynaLoader);
 @EXPORT = qw( filter_add filter_del filter_read filter_read_exact) ;
-$VERSION = "1.56_02" ;
+$VERSION = "1.57" ;
 $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
